f5e9ff10b3ca8999a82a35e87daf94ef1ccd0461
AndrewBubnov <andrew.bubnov75@gmail.com>
2018-05-01 17:22:08 +0300

    Writing to the file from 10 threads

---
+++ src/main/java/InputOutput.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class InputOutput extends Thread{
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            InputOutput inputOutput = new InputOutput();
+            inputOutput.start();
+            String string = "Andrew Bubnov - " + i + " thread\n";
+            InputStream stream = new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8));
+            try (Writer writer = new FileWriter("D:\\test.txt\\", true)) {
+
+                int c = stream.read();
+                while (c != -1) {
+                    writer.write(c);
+                    c = stream.read();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    @Override
+    public void run(){
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+
+
+
