b4238323be4117d383bb48f1452f11dd1acdcbdb
AndrewBubnov <andrew.bubnov75@gmail.com>
2018-05-02 14:47:45 +0300

    Writing to the file from 10 threads

--- src/main/java/InputOutput.java
+++ src/main/java/InputOutput.java
@@ -6,8 +6,7 @@ public class InputOutput extends Thread{
         for (int i = 0; i < 10; i++) {
             InputOutput inputOutput = new InputOutput();
             inputOutput.start();
-            String string = "Andrew Bubnov - " + i + " thread\n";
-            InputStream stream = new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8));
+            Reader stream = new StringReader("Andrew Bubnov - " + i + " поток\n");
             try (Writer writer = new FileWriter("D:\\test.txt\\", true)) {
                 int c = stream.read();
                 while (c != -1) {
@@ -19,7 +18,6 @@ public class InputOutput extends Thread{
             }
         }
     }
-
     @Override
     public void run(){
         System.out.println(Thread.currentThread().getName());
