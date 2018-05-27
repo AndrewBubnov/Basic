81fea99ee74acb4bd903f547002c9ac3b85317bc
AndrewBubnov <andrew.bubnov75@gmail.com>
2018-05-27 18:22:36 +0300

    Biggest palindrome of multiplying simple 5-digits numbers

--- src/main/java/FiveDigitsPalindrome.java
+++ src/main/java/FiveDigitsPalindrome.java
@@ -5,47 +5,46 @@ public class FiveDigitsPalindrome {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         List<Long> list = new ArrayList<>();
-        for (long i = 99999; i >= 10000; i--) {
-            if (isSimpleImproved(i)) {
-                list.add(i);
-                for (int j = list.size() - 1; j >= 0; j--) {
-                    long n = i * list.get(j);
-                    if (isPalindrome(n)) {
-                        System.out.println("Biggest FiveDigitsPalindrome is: " + n);
-                        System.out.println("factor#1 = " + i + "; factor#2 = " + list.get(j));
-                        long end = System.currentTimeMillis();
-                        System.out.println("Execution time is: " + (end - start) + "ms");
-                        return;
+        for (long first = 99999; first >= 10000; first--) {
+            if ((first >> 1) << 1 != first) {
+                if (isSimpleImproved(first)) {
+                    list.add(first);
+                    for (int second = list.size() - 1; second >= 0; second--) {
+                        long n = first * list.get(second);
+                        if (isPalindrome(n)) {
+                            System.out.println("Biggest FiveDigitsPalindrome is: " + n);
+                            System.out.println("factor#1 = " + first + "; factor#2 = " + list.get(second));
+                            long end = System.currentTimeMillis();
+                            System.out.println("Execution time is: " + (end - start) + "ms");
+                            return;
+                        }
                     }
                 }
             }
         }
-
     }
 
     private static boolean isPalindrome(long n){
-        char[] arr = (n + "").toCharArray();
-        boolean f = true;
+        char[] arr = String.valueOf(n).toCharArray();
         int len = arr.length;
-        for (int i = 0; i < len; i++) {
+        int i;
+        for (i = 0; i < len; i++) {
             if (arr[i] != arr[len - 1 - i]) {
-                f = false;
                 break;
             }
         }
-        return f;
+        return i == len;
     }
 
 
-    private static boolean isSimpleImproved(long n) {
-        long i = 3;
-        if ((n / 2) * 2 != n) {
-            for (i = 3; i < n / 2 + 1; i = i + 2) {
-                if ((n / i) * i == n) {
+    private static boolean isSimpleImproved(long number) {
+        long i;
+        int finish = (int)(number>>1) + 1;
+            for (i = 3; i < finish; i = i + 2) {
+                if ((number / i) * i == number) {
                     break;
                 }
             }
-        }
-        return i == n / 2 + 1;
+        return i == finish;
     }
 }
