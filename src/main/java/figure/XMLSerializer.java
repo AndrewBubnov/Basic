c05e24c8fc83b01f450118c41b5555a318b2dab2
AndrewBubnov <andrew.bubnov75@gmail.com>
2018-05-31 09:02:47 +0300

    Serializing figures and figureLists to XML

--- src/main/java/figure/XMLSerializer.java
+++ src/main/java/figure/XMLSerializer.java
@@ -12,11 +12,10 @@ public class XMLSerializer implements Serializer {
 
     @Override
     public void serialize(Figure figure) {
-        if ( figure instanceof FigureList) {
+        if ( figure.getClass().equals(FigureList.class)) {
             printFigureListHeader((FigureList) figure);
-        } else if ( figure instanceof Figure) {
+        } else
             printFigure(figure);
-        } else System.out.println("Given object can not be serialized by XMLSerializator");
     }
 
     private void printFigure(Figure figure) {
@@ -46,8 +45,6 @@ public class XMLSerializer implements Serializer {
             String s = "<FigureList total area = " + "\"" + figureList.getArea() + "\">\n";
             file.write(s.getBytes("UTF8"));
             printFigureList(figureList, 0);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -58,8 +55,6 @@ public class XMLSerializer implements Serializer {
             file.seek(file.length());
             xmlTagRemove(file);
             file.write("</FigureList>\n</xml>".getBytes("UTF8"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -74,7 +69,7 @@ public class XMLSerializer implements Serializer {
             printFigureList(figureList, index + 1);
         }
 
-    private RandomAccessFile xmlTagRemove(RandomAccessFile file){
+    private void xmlTagRemove(RandomAccessFile file){
         try {
             file.seek(file.length() - 6);
             int data = file.read();
@@ -90,10 +85,9 @@ public class XMLSerializer implements Serializer {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return file;
     }
 
-    public static void createNewFile() {
+    static void createNewFile() {
         try {
             Files.write(Paths.get("d:\\serialize.xml"),
                     Collections.singleton("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"), Charset.forName("UTF-8"));
